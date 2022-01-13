4cbc6ccd7e2549daa4901411260d363636e960a8
gloz0315 <80665963+gloz0315@users.noreply.github.com>
2022-01-13 15:47:15 +0900

    Create bakjoon_4344.py

---
+++ bakjoon_bronze/bakjoon_4344/bakjoon_4344.py
@@ -0,0 +1,19 @@
+# 첫번째 내가 혼자서 한 문제
+# 평균을 넘는 학생들의 퍼센트를 구하는 것이다.
+# 첫번째 입력값은 몇번 테스트를 돌리는 것인지
+# 두번째 입력값부터는 총 학생수, 그리고 학생들의 점수들을 띄어쓰기 별로 나눠서 쓰면 된다.
+
+C = int(input())
+
+for i in range(C):
+    lst = list(map(int,input().split()))
+    for j in range(1,len(lst)):
+        sum += lst[j]
+    avg = sum/lst[0]
+    for k in range(1,len(lst)):
+        if lst[k] > avg:
+            count += 1
+    total = count/lst[0]*100
+    print(f"{total:.3f}%")
+    count,sum,avg,total = 0,0,0,0
+    lst = []
