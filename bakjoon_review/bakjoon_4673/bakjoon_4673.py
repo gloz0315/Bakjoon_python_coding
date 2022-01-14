b9317f60060e3b1783cfb4208dc355b0772eee85
gloz0315 <80665963+gloz0315@users.noreply.github.com>
2022-01-14 22:39:39 +0900

    Create bakjoon_4673.py

---
+++ bakjoon_review/bakjoon_4673/bakjoon_4673.py
@@ -0,0 +1,14 @@
+# 셀프 넘버 문제
+
+natural_num = set(range(1,10001))
+generated_num = set()
+
+for i in range(1,10001):
+    for j in str(i):
+        i += int(j)
+    generated_num.add(i)
+self_num = sorted(natural_num - generated_num)
+for i in self_num:
+    print(i)
+    
+# 다시 풀어보기 (주말에)
